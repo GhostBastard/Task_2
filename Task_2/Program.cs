ad5f8f7d81652647a61daa5720f3d3e56c092a39
GhostBastard <makarchenko737@gmail.com>
2022-08-25 23:06:43 +0300

    The task is completed

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_2
 {
     internal class Program
     {
+        public static int[] Duplkiller(int[] sortedArr)
+        {
+            List<int> temp = new List<int>();
+            for (int i=0; i<sortedArr.Length; i++)
+            {
+                if (!temp.Contains(sortedArr[i]))
+                    temp.Add(sortedArr[i]);
+                else continue;
+            }
+            int[] result = new int[temp.Count];
+                return result = temp.ToArray();
+        }
+
         static void Main(string[] args)
         {
+            int[] sortedArr = new int[] {1,2,3,4,4,56};
+            foreach (var i in Duplkiller(sortedArr))
+                Console.Write(i + " ");
+            Console.ReadKey();
         }
     }
 }
